dee5af6791ab06e8c0e6989b9949efe8fb2b300e
Μάρκος Γιακουμής <mgiako02@ucy.ac.cy>
2026-01-16 20:17:07 +0200

    Fix empty config and add dynamic game version error message

--- src/server/main.js
+++ src/server/main.js
@@ -4,7 +4,7 @@ const config = JSON.parse(loadTextFile('config.json'));
 
 bindEventHandler('onResourceStart', thisResource, (event, resource) => {
     if (server.game != GAME_GTA_IV) {
-        console.error('[crime-resource] This resource only supports GTA IV.');
+        console.error(`[${resource.name}] Unfortunately, only GTA IV is supported.`);
         resource.stop();
 
         return;
